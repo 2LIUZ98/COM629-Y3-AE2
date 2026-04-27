a4f5e7d3238ef200b00da5e2200483a25297170e
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-04-27 22:34:33 +0100

    FRONTEND: COMPONETS - INTEGRATING INDVIDUAL FILTER REST BUTTON TO APPLY BUTTON

--- frontend/src/components/filterApplyButton.jsx
+++ frontend/src/components/filterApplyButton.jsx
@@ -1,12 +1,23 @@
-export default function ApplyButton({ onClick }) {
+export default function ActionButtons({ onApply, onReset }) {
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex gap-2">
+      
       <button
-        onClick={onClick}
-        className="bg-[#05339C] text-white px-4 py-2 rounded border border-[#05339C] transition hover:bg-white hover:text-[#05339C]"
+        onClick={onApply}
+        className="w-1/2 bg-[#05339C] text-white px-4 py-2 rounded border border-[#05339C]
+                   transition hover:bg-white hover:text-[#05339C]"
       >
         Apply
       </button>
+
+      <button
+        onClick={onReset}
+        className="w-1/2 bg-white text-[#05339C] px-4 py-2 rounded border border-[#05339C]
+                   transition hover:bg-[#05339C] hover:text-white"
+      >
+        Reset
+      </button>
+
     </div>
   );
 }
