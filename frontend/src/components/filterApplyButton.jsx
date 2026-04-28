c73e91d5f9a310cc6878cc1f19ea0b3471a6afc1
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-04-28 15:12:52 +0100

    FRONTENED: COMPONENTS - RESET BUTTON WIDTH CHANGE

--- frontend/src/components/filterApplyButton.jsx
+++ frontend/src/components/filterApplyButton.jsx
@@ -1,10 +1,10 @@
 export default function ResetButton({ onReset }) {
   return (
-    <div className="flex gap-4">
+    <div className="flex gap-2">
 
       <button
         onClick={onReset}
-        className="w-1/2 bg-white text-[#05339C] px-4 py-2 rounded border border-[#05339C]
+        className="w-full bg-white text-[#05339C] px-4 py-2 rounded border border-[#05339C]
                    transition hover:bg-[#05339C] hover:text-white"
       >
         Reset
