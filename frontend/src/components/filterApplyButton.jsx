a1dbfb8bf6cdd8cc3f58e22c417ece6f29e8554c
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-04-28 14:55:41 +0100

    FRONTENED: COMPONENTS - ACTION BUTTONS TO RESET BUTTON

--- frontend/src/components/filterApplyButton.jsx
+++ frontend/src/components/filterApplyButton.jsx
@@ -3,14 +3,6 @@ export default function ActionButtons({ onApply, onReset }) {
     <div className="flex gap-2">
       
       <button
-        onClick={onApply}
-        className="w-1/2 bg-[#05339C] text-white px-4 py-2 rounded border border-[#05339C]
-                   transition hover:bg-white hover:text-[#05339C]"
-      >
-        Apply
-      </button>
-
-      <button
         onClick={onReset}
         className="w-1/2 bg-white text-[#05339C] px-4 py-2 rounded border border-[#05339C]
                    transition hover:bg-[#05339C] hover:text-white"
